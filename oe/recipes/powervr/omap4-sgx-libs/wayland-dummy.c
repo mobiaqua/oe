374b4cccd2da6f0f9b9082010811431bb56e329d
Paweł Kołodziejski <aquadran@users.sourceforge.net>
2019-12-27 14:23:46 +0100

    Cleanup wayland stubs

--- oe/recipes/powervr/omap4-sgx-libs/wayland-dummy.c
+++ oe/recipes/powervr/omap4-sgx-libs/wayland-dummy.c
@@ -1,12 +1,6 @@
 struct {
-	const char *v1;
-	int v2;
-	int v3;
-	void *v4;
-	int v5;
-	void *v6;
+    void (*destroy)(void *a1, void *a2);
 } wl_buffer_interface;
-void wl_resource_post_error(void *a1, unsigned int a2, const char *a3, ...) {}
 void wl_resource_destroy(void *a1) {}
 unsigned int wl_client_add_resource(void *a1, void *a2) { return 0; }
 void *wl_display_add_global(void *a1, void *a2, void *a3, void *a4) { return 0; }
